b5bd3d400f54bb002c873e5f28032c86ee2dfb95
João Victor Rocha Melo <joao.rochamelo1993@gmail.com>
2018-04-03 18:14:49 -0300

    Atualizações de template

--- header.php
+++ header.php
@@ -1,1 +1,49 @@
-header.php
+<!doctype html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+        <!--Main CSS-->
+        <link rel="stylesheet" href="css/style.css">
+
+        <title>JRocha Designer</title>
+
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/solid.css" integrity="sha384-v2Tw72dyUXeU3y4aM2Y0tBJQkGfplr39mxZqlTBDUZAb9BGoC40+rdFCG0m10lXk" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/regular.css" integrity="sha384-A/oR8MwZKeyJS+Y0tLZ16QIyje/AmPduwrvjeH6NLiLsp4cdE4uRJl8zobWXBm4u" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/brands.css" integrity="sha384-IiIL1/ODJBRTrDTFk/pW8j0DUI5/z9m1KYsTm/RjZTNV8RHLGZXkUDwgRRbbQ+Jh" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/fontawesome.css" integrity="sha384-q3jl8XQu1OpdLgGFvNRnPdj5VIlCvgsDQTQB6owSOHWlAurxul7f+JpUOVdAiJ5P" crossorigin="anonymous">
+    </head>
+    <body>
+        <!--HEADER-->
+        <header>
+            <!-- <img src="img/main-header.jpg" class="img-fluid"> -->
+        </header><!--END HEADER-->
+
+        <!--MENU-->
+        <nav class="navbar sticky-top navbar-light navbar-expand-lg">
+            <div class="container">
+                <a class="navbar-brand" href="#">
+                    <img src="img/logo.png" width="134" height="134" alt="" class="logo">
+                  </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <h2 class="text-menu"><span class="color-light">João Rocha</span> UX/UI "Merc' with a mouth" designer</h2>
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="work.php">WORK <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">CONTACT</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!--END MENU-->
---
+++ index.php
@@ -0,0 +1,119 @@
+<?php 
+	include_once ('header.php');
+?>
+
+<div class="container">
+            <!--JOBS-->
+            <div id="jobs">
+                <div class="row justify-content-center my-3">
+                    <div class="col-6">
+                        <div class="section-title">
+                            <h1 class="text-center">JRocha Designer's WORKS</h1>
+                            <h5 class="subtitle text-center">I design and develop functional, creative, details and usability oriented websites as well as web applications by exposing the simplicity and the lastest web standards as the main principies.</h5>
+                        </div>
+                    </div>
+                </div>
+                <div class="row my-5">
+                    <div class="col">
+                        <div class="grid">
+                            <figure class="effect-apollo">
+                                <img src="img/18.jpg" alt="img18"/>
+                                <figcaption>
+                                    <h2>Strong <span>Apollo</span></h2>
+                                    <p>Wordpress<br>Responsive<br>Web Design<br>E-Commerce</p>
+                                    <a href="#">View more</a>
+                                </figcaption>           
+                            </figure>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="grid">
+                            <figure class="effect-apollo">
+                                <img src="img/22.jpg" alt="img18"/>
+                                <figcaption>
+                                    <h2>Strong <span>Apollo</span></h2>
+                                    <p>Wordpress<br>Responsive<br>Web Design<br>E-Commerce</p>
+                                    <a href="#">View more</a>
+                                </figcaption>           
+                            </figure>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="grid">
+                            <figure class="effect-apollo">
+                                <img src="img/18.jpg" alt="img18"/>
+                                <figcaption>
+                                    <h2>Strong <span>Apollo</span></h2>
+                                    <p>Wordpress<br>Responsive<br>Web Design<br>E-Commerce</p>
+                                    <a href="#">View more</a>
+                                </figcaption>          
+                            </figure>
+                        </div>
+                    </div>
+                    <div class="w-100 my-3"></div>
+                    <div class="col">
+                        <div class="grid">
+                            <figure class="effect-apollo">
+                                <img src="img/18.jpg" alt="img18"/>
+                                <figcaption>
+                                    <h2>Strong <span>Apollo</span></h2>
+                                    <p>Wordpress<br>Responsive<br>Web Design<br>E-Commerce</p>
+                                    <a href="#">View more</a>
+                                </figcaption>           
+                            </figure>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="grid">
+                            <figure class="effect-apollo">
+                                <img src="img/22.jpg" alt="img18"/>
+                                <figcaption>
+                                    <h2>Strong <span>Apollo</span></h2>
+                                    <p>Wordpress<br>Responsive<br>Web Design<br>E-Commerce</p>
+                                    <a href="#">View more</a>
+                                </figcaption>           
+                            </figure>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="grid">
+                            <figure class="effect-apollo">
+                                <img src="img/18.jpg" alt="img18"/>
+                                <figcaption>
+                                    <h2>Strong <span>Apollo</span></h2>
+                                    <p>Wordpress<br>Responsive<br>Web Design<br>E-Commerce</p>
+                                    <a href="#">View more</a>
+                                </figcaption>           
+                            </figure>
+                        </div>
+                    </div>
+                </div>
+            </div><!--END JOBS-->
+            
+            <!--SKILLS-->
+            <div id="skills">
+                <div class="row justify-content-center my-3">
+                    <div class="col-6">
+                        <div class="section-title">
+                            <h2 class="text-center">My Skills</h2>
+                            <h5 class="text-center">During more than 5 years in the web industry, I've worked with many different people on various projects and as a result I gained some unique and strong experience in the IT field and generally - life.</h5>
+                        </div>
+                    </div>
+                </div>
+                <div class="row my-5">
+                    <div class="col-4">
+                        <div id="skill1" class="skill"></div>
+                    </div>
+                    <div class="col-4">
+                        <div id="skill2" class="skill"></div>
+                    </div>
+                    <div class="col-4">
+                        <div id="skill3" class="skill"></div>
+                    </div>
+                </div>
+            </div><!--END SKILLS-->
+        </div>
+
+<?php
+	include_once ('footer.php');
+?>
